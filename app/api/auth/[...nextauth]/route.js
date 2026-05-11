169569d00e64dde5e0dcc654c04ff0ae1344ecdb
Delowar Hossain <31078651+imkhaled404@users.noreply.github.com>
2026-05-12 00:01:16 +0600

    final

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -60,6 +60,7 @@ export const authOptions = {
         strategy: 'jwt',
     },
     secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env.NODE_ENV === 'development' || true,
 };
 
 const handler = NextAuth(authOptions);
